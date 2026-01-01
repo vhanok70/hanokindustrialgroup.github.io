7e20a875880a4efbf2d13683722944f3c529f13f
vhanok70 <vhanok70@gmail.com>
2026-01-01 15:31:37 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,24 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("contact-form");
+
+  emailjs.init("q8jA1T_Q8N19cTKbw");
+
+  const form = document.getElementById("inquiryForm");
+
+  if (!form) {
+    console.error("Form not found");
+    return;
+  }
 
   form.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_qvezi7w",
-        "template_zgmufr1",
-        this
-      )
-      .then(
-        function () {
-          alert("Inquiry sent successfully. We will contact you shortly.");
-          form.reset();
-        },
-        function (error) {
-          alert("Failed to send inquiry. Please try again.");
-          console.error("EmailJS Error:", error);
-        }
-      );
+    emailjs.sendForm(
+      "service_qvezi7w",
+      "template_zgmufr1",
+      this
+    ).then(
+      function () {
+        alert("Inquiry sent successfully. We will contact you shortly.");
+        form.reset();
+      },
+      function (error) {
+        alert("Failed to send inquiry. Please try again.");
+        console.error("EmailJS Error:", error);
+      }
+    );
   });
+
 });
